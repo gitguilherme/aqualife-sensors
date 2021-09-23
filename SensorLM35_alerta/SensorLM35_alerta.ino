925886386ffea168f1e951378eb2365307af3241
NiccolasMS <nicolas.santos@bandtec.com.br>
2021-09-22 22:40:12 -0300

    Adicionando sensor LM35_Alerta

---
+++ SensorLM35_alerta/SensorLM35_alerta.ino
@@ -0,0 +1,36 @@
+int pinoSensor = 5; // Saida do sensor na A0. 
+int valorLido = 0; //Variável auxiliar.
+float temperatura = 0; //Variável que armazenará a temperatura lida 
+int linha =0; //variavel que se refere as linhas do excel
+
+void setup() {//Função que será executada uma
+Serial.begin(9600); //Inicia a comunicação serial a 9600 bauds.
+Serial.println("CLEARDATA"); //reset comunicação serial 
+Serial.println("Temperatura, linha"); //nomeia a coluna 
+}
+void loop() {//Função que será executada continuamente.
+valorLido = analogRead(pinoSensor);//Leitura analógica da porta A0
+temperatura = (valorLido * 0.00488);// 5 volts/ 1023 = 0,0048 precisão do A/D 
+temperatura = temperatura * 100;// converte milivolts para celsius - cada 10mV == 1 grau C
+linha++;//incrementa linha para que a leitura pule linha 
+Serial.print(temperatura);
+Serial.print(",");
+Serial.println(linha);
+
+if(temperatura > 29){
+  Serial.println("Temperatura CRITICA!!!");
+}
+else if(temperatura > 24){
+  Serial.println("Temperatura OK!");
+}
+else{
+  Serial.println("Temperatura CRITICA!!!");
+}
+  
+if(linha > 100)// loop para limitar a qte de dados
+{
+linha = 0;
+Serial.println("ROW, SET,2");//alimentação das linhas sempre com os dados iniciados
+}
+delay(100); //Tempo 5 seg para realizar outra leitura.
+}
